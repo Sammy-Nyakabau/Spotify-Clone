d13ef4b1bc8270224ca258b2a3d60221e60884ab
Sammy Nyakabau <nyakabausammy8@gmail.com>
2020-09-20 19:51:33 +0530

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+ /* eslint-disable */ 
+import React from "react";
+import "../styles/Header.css";
+import { useStateValue } from "../providers/StateProvider";
+import { Avatar } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import { useEffect } from "react";
+
+function Header({ spotify }) {
+  const [{ user }, dispatch] = useStateValue();
+
+  return (
+    <div className="header">
+      <div className="header__left">
+        <SearchIcon />
+        <input
+          placeholder="Search for Artists, Songs, or Podcasts "
+          type="text"
+        />
+      </div>
+      <div className="header__right">
+        <Avatar alt={user?.display_name} src={user?.images[0].url} />
+        <h4>{user?.display_name}</h4>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
